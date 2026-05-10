64e6af89852cc6b0d816ee2ca798695520615402
sua23sua <reconco13@gmail.com>
2026-05-10 21:01:13 +0200

    Fix Stripe API version compatibility

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-04-30.basil",
-});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export const PLANS = [
   {
